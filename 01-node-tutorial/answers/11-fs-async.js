e37d9b245f042f86a1c5fdffadb74a9d32f88cf4
AE/BHH <ehsaniabbas9@gmail.com>
2024-12-08 16:41:26 -0700

    Last changes to file 11 for week1 assignment

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -24,8 +24,14 @@ writeFile(
 							'Hello, Sam!\n',
 							{ flag: 'a' },
 							(err, res) => {
-								console.log('works!')
-								console.log('Something went wrong', err)
+								console.log('It works!')
+								readFile('./temporary/fileB.txt', 'utf8', (err, res) => {
+									if (err) {
+										console.log(err)
+									} else {
+										console.log(res)
+									}
+								})
 							}
 						)
 					}
@@ -34,11 +40,3 @@ writeFile(
 		}
 	}
 )
-
-readFile('./temporary/fileB.txt', 'utf8', (err, res) => {
-	if (err) {
-		console.log(err)
-	} else {
-		console.log(res)
-	}
-})
