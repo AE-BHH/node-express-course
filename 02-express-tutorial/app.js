0d08d593c73cfd5f6c1ec2f4abdb463eb32eb700
AE/BHH <ehsaniabbas9@gmail.com>
2024-12-15 23:08:07 -0700

    Week4 Assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,63 +1,49 @@
 const express = require('express')
 const app = express()
-const { products } = require('./data')
+const peopleRouter = require('./routes/people')
 
-app.use(express.static('./public'))
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use('/api/v1/people', peopleRouter)
 
-app.get('/api/v1/products', (req, res) => {
-	const newProducts = products.map((item) => {
-		const { id, name, price } = item
-		return { id, name, price }
-	})
-	res.json(newProducts)
-})
+// const logger = (req, res, next) => {
+// 	const method = req.method
+// 	const url = req.url
+// 	const time = new Date().getFullYear()
+// 	console.log(method, url, time)
+// 	next()
+// }
+// //middleware
+// app.use(logger)
 
-app.get('/api/v1/products/:productID', (req, res) => {
-	console.log(res)
-	const { productID } = req.params
+// app.get('/', logger, (req, res) => {
+// 	res.send('HOME PAGE')
+// })
 
-	const productByID = products.find(
-		(product) => product.id === parseInt(productID)
-	)
-	if (!productByID) {
-		return res.status(404).json({ message: 'Product not found!' })
-	} else {
-		res.json(productByID)
-	}
-})
+// app.get('/api/v1/postman/people', logger, (req, res) => {
+// 	res.status(200).json({ success: true, data: people })
+// })
 
-app.get('/api/v1/query', (req, res) => {
-	const { search, limit, price } = req.query
-	let sortedProducts = [...products]
+// app.post('/api/v1/postman/people', (req, res) => {
+// 	const { name } = req.body
+// 	if (!name) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, msg: 'Please provide a name' })
+// 	}
+// 	people.push({ id: people.length + 1, name: req.body.name })
+// 	res.status(201).json({ success: true, name: req.body.name })
+// })
 
-	if (price) {
-		sortedProducts = sortedProducts.filter((product) => {
-			const productPrice = product.price
-			console.log(productPrice)
-			return productPrice <= parseFloat(price)
-		})
-	}
+// app.post('/login', (req, res) => {
+// 	const { name } = req.body
 
-	if (search) {
-		sortedProducts = sortedProducts.filter((product) => {
-			// console.log(product)
-			return product.name.startsWith(search)
-		})
-	}
-	if (limit) {
-		sortedProducts = sortedProducts.slice(0, parseInt(limit))
-	}
-
-	if (sortedProducts.length < 1) {
-		return res.status(200).json([])
-	}
-
-	res.status(200).json(sortedProducts)
-})
-
-app.all('*', (req, res) => {
-	res.status(404).send('Not Found!')
-})
+// 	if (name) {
+// 		return res.status(200).send(`Welcome, ${name}`)
+// 	}
+// 	res.status(401).send('Please provide credentials')
+// })
 
 const PORT = 3000
 app.listen(PORT, () => {
